e6a98fa7872ddfcfb16b4c5361ae95ca233768c5
root <root@localhost.localdomain>
2014-02-13 15:31:31 +0800

    template_base

---
+++ nginxlog/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from nginxlog.models import Device
+
+admin.site.register(Device)
--- nginxlog/models.py
+++ nginxlog/models.py
@@ -1,3 +1,12 @@
+#!coding:utf8
 from django.db import models
 
-# Create your models here.
+class Device(models.Model):
+    nip = models.CharField(max_length=30)
+    wip = models.CharField(max_length=50, null=True, blank=True)
+    hostname = models.CharField(max_length=50, null=True, blank=True)
+    status = models.CharField(max_length=1, choices=(('y','是'),('n','否')))
+    rack = models.CharField(max_length=10)
+
+    def __unicode__(self):
+        return self.hostname
